e7383415977e73a0be2c0d31178183d175d151f5
dev vyas <devendravyas04@gmail.com>
2023-01-19 11:21:39 +0000

    Pushing the project to git

---
+++ js/custom.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+
+
+    /* **** scrollIt ***** */
+    $(function () {
+        $.scrollIt({
+            upKey: 38,
+            downKey: 40,
+            easing: "linear",
+            scrollTime: 600,
+            activeClass: "active",
+            onPageChange: null,
+            topOffset: -80,
+        });
+    });
+    /* **** End scrollIt ***** */
+
+
+
+    /* **** Navigation Toggle Start **** */
+    $(".navbar-collapse a").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+    /* **** Navigation Toggle End **** */
+
+
+
+    /* **** sticky **** */
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 150) {
+            $("header").addClass("nav-new");
+        } else {
+            $("header").removeClass("nav-new");
+        }
+    });
+    /* **** sticky **** */
+
+
+
+    /* **** AOS **** */
+    AOS.init({
+        duration: 1000,
+    });
+    /* **** End AOS **** */
+    
+});
